6a2d725f97a7cfea68c16c6e99ef0a010616bbcc
MoonTeam01 <67573296+MoonTeam01@users.noreply.github.com>
2020-08-07 14:58:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://nobi-bot.glitch.me"]
+const urls = ["https://nobi2.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
