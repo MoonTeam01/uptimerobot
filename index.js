30b2b11373b5f0fdcdc29ff42975307f951aced4
Nobi <67573296+iBlueFox@users.noreply.github.com>
2020-08-10 16:08:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://nobi2.glitch.me"]
+const urls = ["https://bluefox.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
