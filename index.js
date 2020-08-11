cc3331f4e4644bb2c2b7bcfb34350369a598c2fb
Nobi <67573296+iBlueFox@users.noreply.github.com>
2020-08-11 18:47:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://nobi2.glitch.me"]
+const urls = ["https://quiet-elastic-kosmoceratops.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
